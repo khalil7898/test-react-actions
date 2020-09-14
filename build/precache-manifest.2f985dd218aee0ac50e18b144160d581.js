e8010583f9e9d1fb38af1e90b6ded23589f2a4df
khalil7898 <khalilzouari7898@gmail.com>
2020-09-14 16:11:25 +0100

    add build

---
+++ build/precache-manifest.2f985dd218aee0ac50e18b144160d581.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c9692fb2155b02668c863ffc4cf240",
+    "url": "/index.html"
+  },
+  {
+    "revision": "705a51fb37a8477c5716",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9fd98578f65f3cbc35ce",
+    "url": "/static/js/2.bf68e547.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf68e547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "705a51fb37a8477c5716",
+    "url": "/static/js/main.9f79c48e.chunk.js"
+  },
+  {
+    "revision": "5ca023aaeca89b846a24",
+    "url": "/static/js/runtime-main.be969eb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
